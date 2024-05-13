ce9ad330167c009adb9d7974873f9d540fdcb425
WayneYam <dd490mp@gmail.com>
2024-05-13 19:34:22 +0800

    tried to rework tidiness score

--- src/ai.cpp
+++ src/ai.cpp
@@ -14,6 +14,7 @@ void PlayerAI::event_handler(sf::Event event){
     if (event.type == sf::Event::KeyPressed){
         if(event.key.code == get_keybind(Keybind::reset))      reset();
         if(event.key.code == get_keybind(Keybind::hard_drop))  hard_drop();
+        if(event.key.code == get_keybind(Keybind::undo))       undo();
     }
 }
 
@@ -26,8 +27,85 @@ void PlayerAI::reset(){
 bool is_solid(int x){
     return 0 <= x && x < 7;
 }
+
+int tidiness(const Board &B){
+    int evaluation = 100;
+
+    // for(auto i : B) debug(i);
+    // tuck test
+    for(int i = 0; i < 2 * B.N - 1; i++){
+        for(int j = 0; j < B.M; j++) {
+            if(is_solid(B[i+1][j]) && !is_solid(B[i][j])) { // has to tuck
+                evaluation -= 20;
+            }
+        }
+    }
+
+    // hole test
+    for(int i = 1; i < 2 * B.N - 1; i++){
+        for(int j = 0; j < B.M; j++) {
+            if(is_solid(B[i+1][j]) && !is_solid(B[i][j]) && (i == 1 || is_solid(B[i-1][j])) && (i == B.M - 1 || is_solid(B[i+1][j])) ) { // has hole
+                evaluation -= 100;
+            }
+        }
+    }
+
+    // find the stack height
+    std::vector<int> height(B.M);
+    for(int i = 0; i < B.M; i++){
+        for(int j = 0;;j++){
+            if(!is_solid(B[j][i])){
+                height[i] = j;
+                break;
+            }
+        }
+    }
+
+
+    int stack_top = *std::max_element(height.begin() + 1, height.end());
+    int stack_bottom = *std::min_element(height.begin() + 1, height.end());
+    // check line clears;
+    {
+        // count covering well
+        int tmp = 0;
+        for(int i = 0; i < 2 * B.N; i++){
+            tmp += is_solid(B[i][0]);
+        }
+
+        int lines_cleared = 0;
+        for(int i = 0; i < 2 * B.N; i++){
+            bool full = 1;
+            for(int j = 0; j < B.M; j++){
+                if(!is_solid(B[i][j])){
+                    full = 0;
+                    break;
+                }
+            }
+            lines_cleared += full;
+        }
+
+        // debug(tmp, height);
+        if(tmp == 0) {
+            if(stack_top - lines_cleared > 16) evaluation -= 20 * (stack_top - lines_cleared - 16);
+        }
+        else if(tmp != lines_cleared) evaluation -= 100;
+        else if(lines_cleared != 4) evaluation -= 50;
+        else evaluation += 50; // clear the tetris
+    }
+
+    // prefer flat placement
+
+    evaluation -= 5 * (stack_top - stack_bottom);
+    for(int i = 1; i < B.M - 1; i++){
+        evaluation -= std::abs(height[i] - height[i+1]) * std::abs(height[i] - height[i+1]);
+    }
+    // debug(evaluation);
+
+    return evaluation;
+}
+
 int eval(const Board &B){
-    return 0;
+    return tidiness(B);
 }
 
 void PlayerAI::place_a_piece(){
