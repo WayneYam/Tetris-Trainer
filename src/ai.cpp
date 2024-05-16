d942408bf8082c102f845402e7e66b77e13acc39
WayneYam <dd490mp@gmail.com>
2024-05-16 19:54:49 +0800

    tried to analyze dependency

--- src/ai.cpp
+++ src/ai.cpp
@@ -3,7 +3,9 @@
 #include <SFML/System/Sleep.hpp>
 #include <SFML/System/Time.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <algorithm>
 #include <array>
+#include <numeric>
 #include <set>
 #include <queue>
 #include "debug.hpp"
@@ -22,12 +24,77 @@ void PlayerAI::event_handler(sf::Event event){
 
 void PlayerAI::reset(){
     // debug("called reset");
+    std::cout << "put " << piece_count << " pieces\n";
     Player::reset();
     Player::swap();
 }
 
 bool is_solid(int x){
     return 0 <= x && x < 7;
+}
+
+int eval(std::vector<int> v){
+    int evaluation = 0;
+    { // clear only tetris.
+        int mn = *std::min_element(v.begin(), v.end());
+        if(mn != 0){
+            if(mn == 4){ // tetris
+                evaluation += 50;
+            }else{
+                evaluation -= 100;
+            }
+        }
+        if(v[0] != mn) { // 0-9 stacking
+            evaluation -= 2000;
+        }
+    }
+
+
+    v[0] = 100;
+    v.push_back(100);
+
+    std::vector<int> dependency(7); // Z L O S I J T
+    {
+        for(int i = 1; i < (int)v.size() - 1; i++){
+            if(v[i] == std::min({v[i-1], v[i], v[i+1]}) ) {
+                int dl = v[i-1] - v[i], dr = v[i+1] - v[i];
+
+                if(dl >= 3 && dr >= 3){
+                    dependency[4]++; // I piece
+                }else if(dl == 2 && dr >= 3){
+                    dependency[1]++; // L piece
+                }else if(dr == 2 && dl >= 3){
+                    dependency[5]++; // J piece
+                }else if(dr == 2 && dl == 2){
+                    // L J dependency
+                }
+            }
+        }
+
+        for(int i = 0; i + 4 < (int)v.size(); i++){
+            for(int t : {-1, 1})
+                if( (v[i] - v[i+1] == t || v[i] - v[i+1] <= -t) && v[i+1] - v[i+2] == t && v[i+2] - v[i+3] == t) dependency[6]++; // T piece
+        }
+    }
+
+    int dsum = std::accumulate(dependency.begin(), dependency.end(), 0);
+
+    if(dsum > 1) evaluation -= 10;
+    if(dsum > 2) evaluation -= 20;
+    if(dsum > 3) evaluation -= 30;
+
+    for(int i : dependency){
+        if(i > 1) evaluation -= 50;
+        if(i > 2) evaluation -= 200;
+    }
+
+    for(int i = 1; i + 2 < (int)v.size(); i++){
+        int diff = std::abs(v[i] - v[i+1]);
+        evaluation -= diff * diff * 3;
+        // evaluation -= diff * 5;
+    }
+
+    return evaluation;
 }
 
 
@@ -96,7 +163,17 @@ void PlayerAI::place_a_piece(){
         if(touch_ground) { // can hard_drop
             
             // debug("test", test.px, test.py, test.rot, test.t);
-            int current_eval = 0;
+            std::vector<int> height(B2.M);
+            for(int i = 0; i < B2.M; i++){
+                for(int j = 0; j < 2 * B2.N; j++){
+                    if(!is_solid(B2[j][i])){
+                        height[i] = j;
+                        break;
+                    }
+                }
+            }
+            int current_eval = eval(height);
+
             if(best_placement.t == -1 || current_eval > best_eval){
                 best_placement = B2.current_piece;
                 best_eval = current_eval;
@@ -105,13 +182,15 @@ void PlayerAI::place_a_piece(){
         B2.take(B2.current_piece);
     }
 
+    if(best_placement.t == -1) return reset();
+
     if(B.current_piece.t != best_placement.t) swap();
 
     B.take(B.current_piece);
     B.current_piece = best_placement;
     B.put(B.current_piece);
 
-    debug(best_eval);
+    // debug(best_eval);
 
     Player::hard_drop();
 }
